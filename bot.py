84517b9e8d3778501edc33a062740f233d16f2f6
ahmedalep <102513381+ahmedalep@users.noreply.github.com>
2023-01-29 18:43:57 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -52,7 +52,8 @@ class Bot(Client):
             plugins={"root": "plugins"},
             sleep_threshold=15,
         )
-        app = web.AppRunner(await web_server())
+        
+app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"       
         await web.TCPSite(app, bind_address, PORT).start()     
