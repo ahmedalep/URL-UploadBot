8e4e735402511b6fd6248cbddb1deefc23711e20
ahmedalep <102513381+ahmedalep@users.noreply.github.com>
2023-01-29 16:22:13 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = os.environ.get("APP_ID", "")
 
 API_HASH = os.environ.get("API_HASH", "")
 
-BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
  
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('AUTH_USERS', '').split()]
