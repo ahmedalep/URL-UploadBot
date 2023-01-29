547f1982bf8a77e216c9df7ca8876f137072d078
ahmedalep <102513381+ahmedalep@users.noreply.github.com>
2023-01-29 20:33:15 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ class Bot(Client):
             bot_token=BOT_TOKEN,
             workers=200,
             plugins={"root": "plugins"},
-            sleep_threshold=15,
+            sleep_threshold=5,
         )
 
     async def start(self):
